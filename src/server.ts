523f75533e32dee6e62567149666edc5df5405ab
Gabriel Dürr <gabriel.durr@outlook.com>
2022-11-23 18:54:40 -0300

    test

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,6 @@ import "./websockets";
 import dotenv from "dotenv";
 dotenv.config();
 
-const PORT = process.env.PRODUCTION || process.env.LOCAL_HOST;
+const PORT = process.env.SERVER_HOST || process.env.LOCAL_HOST;
 
 serverHttp.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
