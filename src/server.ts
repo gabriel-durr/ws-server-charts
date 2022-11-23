3273f64455848fc2a991fe74a568f5fc3b6a7917
Gabriel Dürr <gabriel.durr@outlook.com>
2022-11-23 19:12:31 -0300

    :recycle: Refactor Code

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,6 @@ import "./websockets";
 import dotenv from "dotenv";
 dotenv.config();
 
-const PORT = process.env.SERVER_HOST || process.env.LOCAL_HOST;
+const PORT = process.env.PORT || 7000;
 
 serverHttp.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
